d703e7664887ce21082484aa46a3a401a360ff81
OtonielG <otonielgomez993@gmail.com>
2026-01-12 20:54:04 -0600

    Adding destroy method for cleanup

--- logic/components/searching_component.js
+++ logic/components/searching_component.js
@@ -1,6 +1,6 @@
 export class SearchingVisualizer {
   constructor(...nums) {
-    this.nums = nums.filter(num => typeof num === 'number');;
+    this.nums = nums.filter((num) => typeof num === "number");
     this.tiles = [];
 
     this.createContainer();
@@ -9,29 +9,34 @@ export class SearchingVisualizer {
   }
 
   createContainer() {
-    this.container = document.createElement('div');
-    this.container.classList.add('container');
+    this.container = document.createElement("div");
+    this.container.classList.add("container");
 
-    this.board = document.createElement('div');
-    this.board.classList.add('board');
+    this.board = document.createElement("div");
+    this.board.classList.add("board");
 
     this.container.append(this.board);
   }
-  
+
   createTiles() {
-    this.tiles = this.nums.map(value => this.createTile(value));
+    this.tiles = this.nums.map((value) => this.createTile(value));
   }
 
   createTile(value) {
-    const el = document.createElement('div');
-    el.classList.add('tile');
+    const el = document.createElement("div");
+    el.classList.add("tile");
     el.textContent = value;
     return el;
   }
 
   renderTiles() {
-    this.tiles.forEach(tile => {
+    this.tiles.forEach((tile) => {
       this.board.append(tile);
-    })
+    });
+  }
+
+  destroy() {
+    this.container?.remove();
+    this.tiles = [];
   }
 }
